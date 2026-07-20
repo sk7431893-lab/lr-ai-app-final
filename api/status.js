f5809d532463f1ad2754221fe4e902822e7be8e7
sk7431893-lab <sk7431893@gmail.com>
2026-07-20 23:44:40 +0530

    Update status.js

--- api/status.js
+++ api/status.js
@@ -4,6 +4,9 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") return res.status(200).end();
-  
-  return res.status(200).json({ status: "ok", message: "API is working" });
+
+  return res.status(200).json({
+    status: "ok",
+    message: "API is working",
+  });
 }
